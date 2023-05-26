09d13fca9e0e73613d983317aed8b2392df2f404
Kirans-143 <kiran.shet77@gmail.com>
2023-05-26 19:02:57 +0530

    Created NOTIFICATION_SERVICE for crm app

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Notification_Service
+
+# npm init
+
+# npm i nodemon
+
+# npm i mongoose
+
+# folder confgi => db.config => for db name and url dynamically &&& serverConfig => For the Port number
+
+# npm i express
+
+# npm i body-parser
+
+# with this express make app.listen to port in the server.js file
+
+# Create model foledr => tickect Notification file
+
+# Controllers => ticketnotificationController
+
+# routes => tickect Notification Routes => for the API
+
+# check the API in POSTMAN
---
+++ controllers/ticketNotification.controller.js
@@ -0,0 +1,46 @@
+const { request } = require("express");
+const tickectNotificationModel = require("../models/ticketNotification.model");
+
+exports.acceptNotificationRequest = async (req, res) => {
+  const notificationObject = {
+    ticketId: req.body.ticketId,
+    subject: req.body.subject,
+    content: req.body.content,
+    recepientEmails: req.body.recepientEmails,
+
+    requester: req.body.requester,
+  };
+  try {
+    const notification = await tickectNotificationModel.create(
+      notificationObject
+    );
+
+    if (notification) {
+      return res.status(201).send({
+        reuestId: notification._id,
+        message:
+          "Request has been accepted. Check status by using the provided requestId",
+      });
+    }
+  } catch (err) {
+    return res.status(500).send({
+      message: "Some internal server error occured",
+    });
+  }
+};
+exports.getNotification = async (req, res) => {
+  try {
+    const notification = await tickectNotificationModel.findOne({
+      _id: req.params.id,
+    });
+    if (notification) {
+      return res.status(200).send(notification);
+    } else {
+      return res.status(200).send("id is invalid");
+    }
+  } catch (err) {
+    return res.status(500).send({
+      message: "Some internal Error",
+    });
+  }
+};
