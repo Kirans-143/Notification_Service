67b31e651f174f7af5e9bc4ad164c1edf369be82
Kirans-143 <kiran.shet77@gmail.com>
2023-06-06 23:42:11 +0530

    Added env variable for DB

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   DB_NAME: "notification_db",
-  DB_URL: "mongodb+srv://kiran007:Welcome1@notification-service.cl4homl.mongodb.net/",
+  DB_URL: process.env.MONGODB_URI || "mongod://localhost/notification_db",
 };
